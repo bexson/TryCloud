47616e4bfbff58ed30a2ccbb83cdd9e9ef0beb35
vbedrik <viktoriyabedrik@gmail.com>
2021-01-29 15:08:24 -0500

    continue practice

--- src/test/java/com/trycloud/Viktoriya/UserStory7.java
+++ src/test/java/com/trycloud/Viktoriya/UserStory7.java
@@ -3,6 +3,7 @@ package com.trycloud.Viktoriya;
 public class UserStory7 {
     public static void main(String[] args) {
         System.out.println("Practice");
+        System.out.println("Hi everyone");
     }
 
     }
