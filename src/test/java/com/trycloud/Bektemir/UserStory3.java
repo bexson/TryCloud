149a338f98a6cf62c3d5398b334a32516bb817a8
Beka <bektemir.abdymomunov@mail.ru>
2021-01-31 01:24:00 -0500

    added credentials, created my login method

--- src/test/java/com/trycloud/Bektemir/UserStory3.java
+++ src/test/java/com/trycloud/Bektemir/UserStory3.java
@@ -1,8 +1,34 @@
 package com.trycloud.Bektemir;
 
-public class UserStory3 {
-    public static void main(String[] args) {
-        System.out.println("Practice");
-        //practice with terminal
+import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.ConfigurationReader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class UserStory3 extends TestBase {
+    //As a user, I should be able to access to Files module.
+    @BeforeClass
+    public void getLoginPage(){
+        driver.get(ConfigurationReader.getProperty("url"));
     }
+
+    public void login(){
+        WebElement userInp = driver.findElement(By.cssSelector("input[name='user']"));
+        WebElement passwordInput = driver.findElement(By.cssSelector("input[id='password']"));
+        WebElement loginButton = driver.findElement(By.cssSelector("input[id='submit-form']"));
+
+        userInp.sendKeys(ConfigurationReader.getProperty("username1"));//User17
+        passwordInput.sendKeys(ConfigurationReader.getProperty("password"));//Userpass123
+        loginButton.click();
+    }
+
+    @Test
+    public void tc_1(){
+        String actualTitle = driver.getTitle();
+    }
+
+
+
 }
---
+++ src/test/java/com/trycloud/tests/base/TestBase.java
@@ -0,0 +1,44 @@
+package com.trycloud.tests.base;
+
+import com.trycloud.utilities.ConfigurationReader;
+import com.trycloud.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+    public abstract class TestBase {
+        //This class created for re-usability of methods
+
+        //               Data for TryCloud WebSite :
+        //It will take all data from configuration.properties using ConfigurationReader class
+        protected final String url = ConfigurationReader.getProperty("url");
+        protected final String username1 = ConfigurationReader.getProperty("username1");
+        protected final String username2= ConfigurationReader.getProperty("username2");
+        protected final String username3 = ConfigurationReader.getProperty("username3");
+        protected final String username4 = ConfigurationReader.getProperty("username4");
+        protected final String password = ConfigurationReader.getProperty("password");
+
+
+        protected static WebDriver driver;
+
+        @BeforeClass // this will run 1 time only before class
+        //setUp chrome driver
+        public void setupClass(){
+            driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
+            assert driver != null; // it is just to get rid of warning
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        }
+
+        @AfterClass // this will run 1 time only after class
+        public void closeClass(){
+            driver.close();
+        }
+
+
+
+
+
+}
