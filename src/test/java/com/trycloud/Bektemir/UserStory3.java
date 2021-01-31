18ea381d250c0e1df4654ba526a0f70b54edada2
Beka <bektemir.abdymomunov@mail.ru>
2021-01-30 21:08:26 -0500

    practice1 commited

--- src/test/java/com/trycloud/Bektemir/UserStory3.java
+++ src/test/java/com/trycloud/Bektemir/UserStory3.java
@@ -3,5 +3,6 @@ package com.trycloud.Bektemir;
 public class UserStory3 {
     public static void main(String[] args) {
         System.out.println("Practice");
+        //practice with terminal
     }
 }
