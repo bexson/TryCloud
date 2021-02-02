3b4f947d57c9847e59b37aeacc7abf2c2a2e21da
Fatih <fatihealan@gmail.com>
2021-02-02 09:12:20 -0500

    Finished working on first user story

--- src/test/java/com/trycloud/fatih/practice/US1.java
+++ src/test/java/com/trycloud/fatih/practice/US1.java
@@ -1,6 +1,8 @@
 package com.trycloud.fatih.practice;
 
 import com.trycloud.tests.base.TestBase;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class US1 extends TestBase {
@@ -8,6 +10,21 @@ public class US1 extends TestBase {
     @Test
     public void loginValid(){
         driver.get(url);
-        
+        driver.findElement(By.id("user")).sendKeys(username1);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("submit-form")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),"http://qa.trycloud.net/index.php/apps/files/?dir=/&fileid=3112");
+    }
+
+    @Test
+    public void loginInvalid(){
+        driver.get(url);
+        driver.findElement(By.id("user")).sendKeys(username1);
+        driver.findElement(By.id("password")).sendKeys(password+"a");
+        driver.findElement(By.id("submit-form")).click();
+
+
+        String actualMessage = driver.findElement(By.className("wrongPasswordMsg")).getText();
+        Assert.assertEquals(actualMessage,"Wrong username or password.");
     }
 }
