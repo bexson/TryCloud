979f7d7e5a68c0a159267d2de099227eb7afbd5b
Fatih <fatihealan@gmail.com>
2021-02-02 08:18:18 -0500

    Created a new branch for sole purpose of practicing, and  added a package with a class in it

--- src/test/java/com/trycloud/fatih/practice/US1.java
+++ src/test/java/com/trycloud/fatih/practice/US1.java
@@ -1,4 +1,13 @@
 package com.trycloud.fatih.practice;
 
-public class US1 {
+import com.trycloud.tests.base.TestBase;
+import org.testng.annotations.Test;
+
+public class US1 extends TestBase {
+
+    @Test
+    public void loginValid(){
+        driver.get(url);
+        
+    }
 }
