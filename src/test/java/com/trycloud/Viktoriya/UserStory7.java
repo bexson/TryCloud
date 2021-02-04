9a36e9b0e0f6700eb63b4b4c401520e3c4279852
vbedrik <viktoriyabedrik@gmail.com>
2021-02-03 23:36:24 -0500

    start working on user story#7

--- src/test/java/com/trycloud/Viktoriya/UserStory7.java
+++ src/test/java/com/trycloud/Viktoriya/UserStory7.java
@@ -1,11 +1,18 @@
 package com.trycloud.Viktoriya;
 
-public class UserStory7 {
-    public static void main(String[] args) {
-        System.out.println("Practice");
-        System.out.println("Hi everyone");
-        System.out.println("one more practice");
+import com.trycloud.tests.base.TestBase;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class UserStory7 extends TestBase {
+   /*   #7 As a user, I should be able to access search
+any item/ users from the homepage*/
+
+    public void login(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
-    }
+}
 
