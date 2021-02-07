01e24a5e48b327c30a6cb4dfeda941a97ac0adf1
Beka <bektemir.abdymomunov@mail.ru>
2021-02-06 23:28:08 -0500

    added sleep method

--- src/test/java/com/trycloud/utilities/BrowserUtils.java
+++ src/test/java/com/trycloud/utilities/BrowserUtils.java
@@ -1,4 +1,27 @@
 package com.trycloud.utilities;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BrowserUtils {
+
+    public static void sleep(long seconds){
+        try {
+            Thread.sleep(seconds*1000);
+        }catch (InterruptedException e){
+            System.out.println("Can't handle with Interrupted exception");
+        }
+    }
+
+
+    //a method for explicit wait uses WebDriverWait class
+    public static void clickOn(WebElement element, WebDriver driver, int timeout) {
+        final WebDriverWait wait = new WebDriverWait(driver, timeout);
+        wait.until(ExpectedConditions.refreshed(
+                ExpectedConditions.elementToBeClickable(element)));
+        element.click();
+    }
+
 }
