f4d868ddd2ed7ce129407654c390322c78e9802a
Fatih <fatihealan@gmail.com>
2021-02-07 08:16:20 -0500

    Added login function to testbase

--- src/test/java/com/trycloud/fatih/practice/US2.java
+++ src/test/java/com/trycloud/fatih/practice/US2.java
@@ -13,14 +13,6 @@ import java.util.List;
 public class US2 extends TestBase {
 
 
-    @Test
-    public void loginValid(){
-        driver.get(url);
-        driver.findElement(By.id("user")).sendKeys(username1);
-        driver.findElement(By.id("password")).sendKeys(password);
-        driver.findElement(By.id("submit-form")).click();
-        Assert.assertEquals(driver.getCurrentUrl(),"http://qa.trycloud.net/index.php/apps/dashboard/");
-    }
 
     @Test
     public void mainModules(){
